eebc0ab6468e77d918b7ddaff89e0d55a5cd0359
Christof Petig <christof.petig@arcor.de>
2021-08-03 22:57:12 +0200

    nicer output

--- src/bin/newton_gauss.rs
+++ src/bin/newton_gauss.rs
@@ -21,16 +21,17 @@ fn d(x: &na::DMatrix<f32>) -> f32 {
     f(x).norm_squared()
 }
 
-fn print_2x2(m: &na::DMatrix<f32>) {
-    println!("[ {} {}", m[(0, 0)], m[(0, 1)]);
-    println!("  {} {} ]", m[(1, 0)], m[(1, 1)]);
-}
+// fn print_2x2(m: &na::DMatrix<f32>) {
+//     println!("[ {} {}", m[(0, 0)], m[(0, 1)]);
+//     println!("  {} {} ]", m[(1, 0)], m[(1, 1)]);
+// }
 
 fn gauss_newton(x0: &na::DMatrix<f32>) -> na::DMatrix<f32> {
     let J = J(&x0);
     //na::DMatrix::from_row_slice(2, 2, &[2.0 * sqrt2, sqrt2, 3.0 * sqrt2, 0.0]);
     let JT = J.transpose();
     let D = JT.clone() * J.clone();
+    let Dinv = D.try_inverse().unwrap();
     // print_2x2(&JT);
     // print_2x2(&J);
     // print_2x2(&D);
@@ -39,7 +40,7 @@ fn gauss_newton(x0: &na::DMatrix<f32>) -> na::DMatrix<f32> {
     // dbg!(d(&x0));
     // dbg!(&J);
     // dbg!(&D);
-    let dvec = -(D.try_inverse().unwrap()) * (JT * f(&x0));
+    let dvec = -(Dinv * (JT * f(&x0)));
     let alpha = 1.0_f32; // why negative?
     let x1 = x0 + alpha * dvec;
     // dbg!(&x1);
@@ -51,6 +52,6 @@ pub fn main() {
     let mut x0 = na::DMatrix::from_column_slice(2, 1, &[10.4, 0.4]);
     for _ in 0..5 {
         x0 = gauss_newton(&x0);
-        dbg!((&x0, d(&x0)));
+        println!("[{} {}] {}", x0[(0,0)],x0[(1,0)], d(&x0));
     }
 }
