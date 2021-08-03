b9ae6d8634095607cabe7a6dc9fe2e7b8fb980a4
Christof Petig <christof.petig@arcor.de>
2021-08-03 22:31:58 +0200

    working gauss newton step

--- src/bin/newton_gauss.rs
+++ src/bin/newton_gauss.rs
@@ -1,3 +1,5 @@
+// see https://en.wikipedia.org/wiki/Gauss%E2%80%93Newton_algorithm
+
 extern crate nalgebra as na;
 
 const equation: [[f32; 2]; 2] = [[2.0_f32, 1.0], [3.0, 0.0]];
@@ -5,26 +7,35 @@ const bias: [f32; 2] = [3.0, 3.0];
 const sqrt2: f32 = 1.414213562_f32; // 2.0_f32.sqrt();
 
 fn f(x: &na::DMatrix<f32>) -> na::DMatrix<f32> {
-    let xmat = na::DMatrix::<f32>::from_fn(2, 2, |r, c| equation[r][c]);
+    let xmat = na::DMatrix::<f32>::from_fn(2, 2, |r, c| equation[c][r]);
     let y = na::DMatrix::from_column_slice(2, 1, &bias);
-    y - xmat * x
+    (xmat * x) - y
 }
 
 fn d(x: &na::DMatrix<f32>) -> f32 {
     f(x).norm_squared()
 }
 
+fn print_2x2(m: &na::DMatrix<f32>) {
+    println!("[ {} {}", m[(0, 0)], m[(0, 1)]);
+    println!("  {} {} ]", m[(1, 0)], m[(1, 1)]);
+}
+
 pub fn main() {
-    let J = na::DMatrix::from_row_slice(2, 2, &[2.0 * sqrt2, 3.0 * sqrt2, sqrt2, 0.0]);
+    let x0 = na::DMatrix::from_column_slice(2, 1, &[0.0, 0.0]);
+    let J = na::DMatrix::from_row_slice(2, 2, &[2.0 * sqrt2, sqrt2, 3.0 * sqrt2, 0.0]);
     let JT = J.transpose();
     let D = JT.clone() * J.clone();
-    let x0 = na::DMatrix::from_column_slice(2,1,&[0.0,0.0]);
-    dbg!(f(&x0));
+    // print_2x2(&JT);
+    // print_2x2(&J);
+    // print_2x2(&D);
+    //    dbg!(&D);
+    // dbg!(f(&x0));
     dbg!(d(&x0));
-    dbg!(&J);
-    dbg!(&D);
-    let dvec = - dbg!(D.try_inverse().unwrap()) * dbg!(JT * f(&x0));
-    let alpha = 1.0_f32;
+    // dbg!(&J);
+    // dbg!(&D);
+    let dvec = -(D.try_inverse().unwrap()) * (JT * f(&x0));
+    let alpha = 1.0_f32; // why negative?
     let x1 = x0 + alpha * dvec;
     dbg!(&x1);
     dbg!(d(&x1));
