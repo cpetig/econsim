c821e9e898f77bc0476c7b43a35ab46bf61f2583
Christof Petig <christof.petig@arcor.de>
2021-08-03 23:35:41 +0200

    equation and bias became local (to change them)

--- src/bin/newton_gauss.rs
+++ src/bin/newton_gauss.rs
@@ -1,3 +1,5 @@
+use na::SMatrix;
+
 // see https://en.wikipedia.org/wiki/Gauss%E2%80%93Newton_algorithm
 
 extern crate nalgebra as na;
@@ -5,27 +7,35 @@ extern crate nalgebra as na;
 const M: usize = 2;
 const N: usize = 2;
 
-const equation: [[f32; N]; M] = [[2.0_f32, 1.0], [3.0, 0.0]];
-const bias: [f32; M] = [3.0, 3.0];
 const sqrt2: f32 = 1.414213562_f32; // 2.0_f32.sqrt();
 
-fn f(x: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, M, 1> {
-    let xmat = na::SMatrix::<f32, M, N>::from_fn(|r, c| equation[r][c]);
-    let y = na::SMatrix::<f32, M, 1>::from_column_slice(&bias);
-    (xmat * x) - y
+fn f(
+    equation: &na::SMatrix<f32, M, N>,
+    bias: &na::SMatrix<f32, M, 1>,
+    x: &na::SMatrix<f32, N, 1>,
+) -> na::SMatrix<f32, M, 1> {
+    (equation * x) - bias
 }
 
 // df_r(x)/dx_c  (oh it is not dependent on x)
-fn J(x: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, M, N> {
-    na::SMatrix::<f32, M, N>::from_fn(|r, c| (sqrt2 * equation[r][c]))
+fn J(equation: &na::SMatrix<f32, M, N>, x: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, M, N> {
+    na::SMatrix::<f32, M, N>::from_fn(|r, c| (sqrt2 * equation[(r, c)]))
 }
 
-fn d(x: &na::SMatrix<f32, N, 1>) -> f32 {
-    f(x).norm_squared()
+fn d(
+    equation: &na::SMatrix<f32, M, N>,
+    bias: &na::SMatrix<f32, M, 1>,
+    x: &na::SMatrix<f32, N, 1>,
+) -> f32 {
+    f(equation, bias, x).norm_squared()
 }
 
-fn gauss_newton(x0: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, N, 1> {
-    let J = J(&x0);
+fn gauss_newton(
+    equation: &na::SMatrix<f32, M, N>,
+    bias: &na::SMatrix<f32, M, 1>,
+    x0: &na::SMatrix<f32, N, 1>,
+) -> na::SMatrix<f32, N, 1> {
+    let J = J(equation, x0);
     let JT = J.transpose();
     let D = JT.clone() * J.clone();
     let Dinv = D.try_inverse().unwrap();
@@ -37,14 +47,14 @@ fn gauss_newton(x0: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, N, 1> {
     // dbg!(d(&x0));
     // dbg!(&J);
     // dbg!(&D);
-    let f_x0 = f(&x0);
+    let f_x0 = f(equation, bias, x0);
     let error0 = f_x0.norm_squared();
     let dvec = -(Dinv * (JT * f_x0));
     let mut alpha = 1.0_f32;
     // line search
     let x1 = loop {
         let x1 = x0 + alpha * dvec.clone();
-        let f_x1 = f(&x1);
+        let f_x1 = f(equation, bias, &x1);
         let error1 = f_x1.norm_squared();
         if error1 < error0 {
             break x1;
@@ -60,9 +70,17 @@ fn gauss_newton(x0: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, N, 1> {
 }
 
 pub fn main() {
+    let equation = na::SMatrix::<f32, M, N>::from_row_slice(&[2.0_f32, 1.0, 3.0, 0.0]);
+    let bias = na::SMatrix::<f32, M, 1>::from_column_slice(&[3.0, 3.0]);
+
     let mut x0 = na::SMatrix::<f32, N, 1>::from_column_slice(&[10.4, 0.4]);
     for _ in 0..5 {
-        x0 = gauss_newton(&x0);
-        println!("[{} {}] {}", x0[(0, 0)], x0[(1, 0)], d(&x0));
+        x0 = gauss_newton(&equation, &bias, &x0);
+        println!(
+            "[{} {}] {}",
+            x0[(0, 0)],
+            x0[(1, 0)],
+            d(&equation, &bias, &x0)
+        );
     }
 }
