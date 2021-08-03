0d5882ff24c2a9942df631455cc9adeb591c72f0
Christof Petig <christof.petig@arcor.de>
2021-08-03 23:25:13 +0200

    static matrices

--- src/bin/newton_gauss.rs
+++ src/bin/newton_gauss.rs
@@ -2,33 +2,30 @@
 
 extern crate nalgebra as na;
 
-const equation: [[f32; 2]; 2] = [[2.0_f32, 1.0], [3.0, 0.0]];
-const bias: [f32; 2] = [3.0, 3.0];
+const M: usize = 2;
+const N: usize = 2;
+
+const equation: [[f32; N]; M] = [[2.0_f32, 1.0], [3.0, 0.0]];
+const bias: [f32; M] = [3.0, 3.0];
 const sqrt2: f32 = 1.414213562_f32; // 2.0_f32.sqrt();
 
-fn f(x: &na::DMatrix<f32>) -> na::DMatrix<f32> {
-    let xmat = na::DMatrix::<f32>::from_fn(2, 2, |r, c| equation[r][c]);
-    let y = na::DMatrix::from_column_slice(2, 1, &bias);
+fn f(x: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, M, 1> {
+    let xmat = na::SMatrix::<f32, M, N>::from_fn(|r, c| equation[r][c]);
+    let y = na::SMatrix::<f32, M, 1>::from_column_slice(&bias);
     (xmat * x) - y
 }
 
 // df_r(x)/dx_c  (oh it is not dependent on x)
-fn J(x: &na::DMatrix<f32>) -> na::DMatrix<f32> {
-    na::DMatrix::<f32>::from_fn(2, 2, |r, c| (sqrt2 * equation[r][c]))
+fn J(x: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, M, N> {
+    na::SMatrix::<f32, M, N>::from_fn(|r, c| (sqrt2 * equation[r][c]))
 }
 
-fn d(x: &na::DMatrix<f32>) -> f32 {
+fn d(x: &na::SMatrix<f32, N, 1>) -> f32 {
     f(x).norm_squared()
 }
 
-// fn print_2x2(m: &na::DMatrix<f32>) {
-//     println!("[ {} {}", m[(0, 0)], m[(0, 1)]);
-//     println!("  {} {} ]", m[(1, 0)], m[(1, 1)]);
-// }
-
-fn gauss_newton(x0: &na::DMatrix<f32>) -> na::DMatrix<f32> {
+fn gauss_newton(x0: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, N, 1> {
     let J = J(&x0);
-    //na::DMatrix::from_row_slice(2, 2, &[2.0 * sqrt2, sqrt2, 3.0 * sqrt2, 0.0]);
     let JT = J.transpose();
     let D = JT.clone() * J.clone();
     let Dinv = D.try_inverse().unwrap();
@@ -63,7 +60,7 @@ fn gauss_newton(x0: &na::DMatrix<f32>) -> na::DMatrix<f32> {
 }
 
 pub fn main() {
-    let mut x0 = na::DMatrix::from_column_slice(2, 1, &[10.4, 0.4]);
+    let mut x0 = na::SMatrix::<f32, N, 1>::from_column_slice(&[10.4, 0.4]);
     for _ in 0..5 {
         x0 = gauss_newton(&x0);
         println!("[{} {}] {}", x0[(0, 0)], x0[(1, 0)], d(&x0));
