183377ccf4beff9dd27ead9291f422752ea16202
Christof Petig <christof.petig@arcor.de>
2021-08-28 00:44:51 +0200

    test own inversion routine

--- src/gauss_newton.rs
+++ src/gauss_newton.rs
@@ -14,7 +14,7 @@ const beta_k: f32 = 0.001; // zero gives GaussNewton, non zero Levenberg-Marquar
 
 const sqrt2: f32 = 1.414213562_f32; // 2.0_f32.sqrt();
 
-fn f<const M: usize,const N: usize>(
+fn f<const M: usize, const N: usize>(
     equation: &na::SMatrix<f32, M, N>,
     bias: &na::SMatrix<f32, M, 1>,
     x: &na::SMatrix<f32, N, 1>,
@@ -23,12 +23,15 @@ fn f<const M: usize,const N: usize>(
 }
 
 // df_r(x)/dx_c  (oh it is not dependent on x)
-fn J<const M: usize,const N: usize>(equation: &na::SMatrix<f32, M, N>, x: &na::SMatrix<f32, N, 1>) -> na::SMatrix<f32, M, N> {
+fn J<const M: usize, const N: usize>(
+    equation: &na::SMatrix<f32, M, N>,
+    x: &na::SMatrix<f32, N, 1>,
+) -> na::SMatrix<f32, M, N> {
     sqrt2 * equation
-//   na::SMatrix::<f32, M, N>::from_fn(|r, c| (sqrt2 * equation[(r, c)]))
+    //   na::SMatrix::<f32, M, N>::from_fn(|r, c| (sqrt2 * equation[(r, c)]))
 }
 
-fn d<const M: usize,const N: usize>(
+fn d<const M: usize, const N: usize>(
     equation: &na::SMatrix<f32, M, N>,
     bias: &na::SMatrix<f32, M, 1>,
     x: &na::SMatrix<f32, N, 1>,
@@ -36,9 +39,7 @@ fn d<const M: usize,const N: usize>(
     f(equation, bias, x).norm_squared()
 }
 
-fn print<const M: usize,const N: usize>(
-    x: &nalgebra::SMatrix<f32,M,N>,
-) {
+fn print<const M: usize, const N: usize>(x: &nalgebra::SMatrix<f32, M, N>) {
     for i in 0..M {
         for j in 0..N {
             print!("{:.3}\t", x[(i, j)]);
@@ -47,7 +48,44 @@ fn print<const M: usize,const N: usize>(
     }
 }
 
-pub fn gauss_newton<const M: usize,const N: usize>(
+fn inv_recurse<const M: usize, const N: usize>(
+    res: &mut SMatrix<f32, N, M>,
+    x: &SMatrix<f32, M, N>,
+    row: usize,
+    destrow: usize,
+    factor: f32,
+    depth: u32,
+) {
+    let r = x.row(row);
+    let sum: f32 = r
+        .iter()
+        .filter_map(|a| if *a > 0.0 { Some(*a) } else { None })
+        .sum();
+    if sum > 0.0 {
+        let factor = factor / sum;
+        for (col, _) in r.iter().enumerate().filter(|(_, val)| **val > 0.0) {
+            res[(col, destrow)] += factor;
+            // println!("{} {} {} {}", depth, col, row, factor);
+            if depth >= 1 {
+                for (row2, value) in x.column(col).iter().enumerate().filter(|(_, v)| **v < 0.0) {
+                    inv_recurse(res, x, row2, destrow, -value * factor, depth - 1);
+                }
+            }
+        }
+    }
+}
+
+fn my_inverse<const M: usize, const N: usize>(
+    x: &nalgebra::SMatrix<f32, M, N>,
+) -> nalgebra::SMatrix<f32, N, M> {
+    let mut res = SMatrix::zeros();
+    for row in 0..M {
+        inv_recurse(&mut res, &x, row, row, 1.0, 5);
+    }
+    res
+}
+
+pub fn gauss_newton<const M: usize, const N: usize>(
     equation: &na::SMatrix<f32, M, N>,
     bias: &na::SMatrix<f32, M, 1>,
     x0: &na::SMatrix<f32, N, 1>,
@@ -73,9 +111,12 @@ pub fn gauss_newton<const M: usize,const N: usize>(
     print(&f_x0.transpose());
     print(&scale2);
     print(&dvec.transpose());
-    print(&(scale2*f_x0).transpose());
-    let svd = nalgebra::linalg::SVD::new(J.clone(), true, true);
-    print(&svd.pseudo_inverse(1e-7));
+    print(&(scale2 * f_x0).transpose());
+    print(&equation);
+    let minv = my_inverse(&equation);
+    print(&(minv*-1.0/sqrt2));
+    // let svd = nalgebra::linalg::SVD::new(J.clone(), true, true);
+    // print(&svd.pseudo_inverse(1e-7));
     let mut alpha = 1.0_f32;
     // line search
     let x1 = loop {
